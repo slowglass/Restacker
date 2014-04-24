c565b5b44388bd9fe758d071cf1e88963e886112
Chris Davies <cdavies@itrsgroup.com>
2014-04-24 18:21:50 +0100

    "Hello World" version

---
+++ Restacker.lua
@@ -0,0 +1,15 @@
+﻿Restacker = {}
+function Restacker.Intro()
+	EVENT_MANAGER:UnregisterForEvent("Restacker",EVENT_PLAYER_ACTIVATED)
+	d("Restacker Loaded")
+    EVENT_MANAGER:UnregisterForEvent("Restacker",EVENT_ADD_ON_LOADED)
+end
+
+function Restacker.Loaded(eventCode, addOnName)
+	if(addOnName ~= "Restacker") then return end
+
+	EVENT_MANAGER:RegisterForEvent("Restacker", EVENT_PLAYER_ACTIVATED,Restacker.Intro)
+    EVENT_MANAGER:UnregisterForEvent("Restacker",EVENT_ADD_ON_LOADED)
+end
+
+EVENT_MANAGER:RegisterForEvent("Restacker", EVENT_ADD_ON_LOADED, Restacker.Loaded)
