650c91a4a8ae80a9e55310e31c94bc5167cd7fb0
Chris Davies <cdavies@itrsgroup.com>
2014-05-05 19:31:44 +0100

    Refactored EN messages  - Added 2 new messages for bank stacking

--- Lang.en.lua
+++ Lang.en.lua
@@ -3,8 +3,10 @@ b["LOADED"] = "Restacker Loaded"
 b["SRC_ITEM"] = "Move Item: <<1>>"
 b["DST_ITEM"] = "Dest Item: <<1>>"
 b["EVALUATION"] = "Restacker Evaluation:"
-b["RESTACK_AVAILABLE"] = "    Available for restacking <<1>>: <<2>>"
-b["RESTACKING"] = "    Restacking duplicate stacks of <<1>>: <<2>>"
+b["RESTACK_AVAILABLE"] = "...Available for restacking <<1>>: <<2>>"
+b["RESTACKING"] = "    Restacked stacks of <<2>> of <<1>>"
+b["MOVED_FROM_1_TO_2"] = "Moved <<2[$d unit/$d units]>> of <<1>> to the bank"
+b["MOVED_FROM_2_TO_1"] = "Moved <<2[$d unit/$d units]>> of <<1>> from the bank"
 b["CMD_DESC"] = 
 	"Restacker Command Interface:\n"..
 	" /rs restack: Restacks your bag"..
