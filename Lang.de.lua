32f976f5afc5d9fbdc6946bbaf8ac7e0e46b6cb9
Chris Davies <cdavies@itrsgroup.com>
2014-05-08 23:55:31 +0100

    Added German Lang bundle  - Big thanks to Dunkare

---
+++ Lang.de.lua
@@ -0,0 +1,24 @@
+-- German Lang bundle Dunkare
+local b = {}
+b["BAG"] = "Tasche"
+b["LOADED"] = "Restacker geladen"
+b["SRC_ITEM"] = "Gegenstand: <<1>>"
+b["DST_ITEM"] = "Ziel: <<1>>"
+b["EVALUATION"] = "Restacker Auswertung:"
+b["RESTACK_AVAILABLE"] = "...verfügbar zum Umstapeln <<1>>: <<2>>"
+b["RESTACKING"] = "    Stapel von <<2>> von <<1>> wurden umgestapelt"
+b["MOVED_FROM_1_TO_2"] = " <<2[$d einheit/$d einheiten]>> von <<1>> wurden in die Bank verschoben"
+b["MOVED_FROM_2_TO_1"] = " <<2[$d einheit/$d einheiten]>> von <<1>> wurden aus der Bank genommen"
+b["CMD_DESC"] = 
+	"Restacker Kommandos:\n"..
+	" /rs restack: Fügt Stapel im Inventar zusammen"..
+	" /rs evaluate: Fügt Stapel im Inventar zusammen"..
+	" /rs inv: Zeigt eine Zusammenfassung des Inventars an"
+b["CMD_ERR"] = "Restacker Fehler: /rs <<1>>"
+b["RESTACK_INV"] = "Umstapeln in Bestand"
+b["RESTACK_BNK"] = "Umstapeln in Bank"
+b["STACK_INV_BNK"] = "Stapel aus dem Bestand der Bank"
+b["STACK_BNK_INV"] = "Stapel von der Bank zum Bestand"
+b["NOTHING_MOVED"] = "Keine Stapel verschoben"
+
+Restacker.langBundle["de"] = b
