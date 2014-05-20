a9e7f28fb09e243d851d9b5d1bc6e0c001c4fc26
Chris Davies <cdavies@itrsgroup.com>
2014-05-20 18:38:14 +0100

    Improved French lang bundle - Thanks Xie@esoui

--- Lang.fr.lua
+++ Lang.fr.lua
@@ -1,24 +1,39 @@
 -- French Lang bundle
--- Translation by Google :)
+
 local b = {}
 b["BAG"] = "Sac: <<1>>"
-b["LOADED"] = "Ré empileur Loaded"
-b["SRC_ITEM"] = "Déplacez article: <<1>>"
+b["LOADED"] = "Restacker Chargé"
+b["SRC_ITEM"] = "Déplacer article: <<1>>"
 b["DST_ITEM"] = "Destinations article: <<1>>"
-b["EVALUATION"] = "Évaluation de ré empileur:"
+b["EVALUATION"] = "Évaluation du Restacker:"
 b["RESTACK_AVAILABLE"] = "...Disponible pour réempilage <<1>>: <<2>>"
-b["RESTACKING"] = "    Piles restacked de <<2>> de <<1>>"
-b["MOVED_FROM_1_TO_2"] = "Proposé <<2[$d unité/$d unités]>> de <<1>> à la banque"
-b["MOVED_FROM_2_TO_1"] = "Proposé <<2[$d unité/$d unités]>> de <<1>> de la banque"
+b["RESTACKING"] = "    Piles réempilées de <<2>> de <<1>>"
+b["MOVED_FROM_1_TO_2"] = "Déplacée(s) <<2[$d unité/$d unités]>> de <<1>> à la banque"
+b["MOVED_FROM_2_TO_1"] = "Déplacée(s) <<2[$d unité/$d unités]>> de <<1>> de la banque"
 b["CMD_DESC"] = 
 	"Restacker Command Interface:\n"..
 	" /rs restack: Restacks your inventory\n"..
 	" /rs show <inv||bank>: Prints out your inventory\n"
 b["CMD_ERR"] = "Restacker Error: /rs <<1>>"
-b["RESTACK_INV"] = "Réempilez en inventaire"
-b["RESTACK_BNK"] = "Réempilez en Banque"
-b["STACK_INV_BNK"] = "Empilez de l'inventaire à la Banque"
-b["STACK_BNK_INV"] = "Empilez de la Banque à l'inventaire"
-b["NOTHING_MOVED"] = "Pas de piles déplacés"
+b["RESTACK_INV"] = "Réempiler en inventaire"
+b["RESTACK_BNK"] = "Réempiler en Banque"
+b["STACK_INV_BNK"] = "Empiler de l'inventaire à la Banque"
+b["STACK_BNK_INV"] = "Empiler de la Banque à l'inventaire"
+b["NOTHING_MOVED"] = "Pas de pile déplacée"
 
+-- Option Pabel Strings
+b["OP:TITLE"] = "Restacker"
+b["OP:GENERAL"] = "Paramètres"
+b["OP:ANNOUNCE_TRANSFERS_LB"] = "Notification des items transférés"
+b["OP:ANNOUNCE_TRANSFERS_TT"] = "Notification lors du réempilage"
+b["OP:AUTO_BANK_TRANSFER_LB"] = "Action à l'ouverture de la Banque"
+b["OP:AUTO_BANK_TRANSFER_TT"] = "Sélectionner l'empilage automatique à exécuter lorsque la Banque est ouverte"
+b["OP:ABT_I2B"] = "Transfert de l'Inventaire à la Banque"
+b["OP:ABT_B2I"] = "Transfert de la Banque à l'Inventaire"
+b["OP:ABT_NONE"] = "Ne Rien Transférer"
+b["OP:AUTO_TRADE_TRANSFER_LB"] = "Empilage auto après échange"
+--b["OP:AUTO_TRADE_TRANSFER_TT"] =
+b["OP:DEBUG"] = "Paramètre Debug"
+b["OP:DEBUG_LB"] = "Afficher les Messages Debug"
+b["OP:DEBUG_TT"] = "Afficher les Messages Debug dans la fenêtre de Chat"
 Restacker.langBundle["fr"] = b
